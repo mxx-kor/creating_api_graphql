6dfe72f45de509d5074fababfb57f1dcfcb7f6a1
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-05-18 19:04:01 +0900

    learned how to write resolvers
    resolver를 이용해 작성된 데이터를 가져오는 법과 mutation의 기능을 작성하여 사용하는 방법에 대해 학습함

--- tweetql/server.js
+++ tweetql/server.js
@@ -1,5 +1,16 @@
 import { ApolloServer, gql } from "apollo-server"
 
+let tweets = [
+    {
+        id: "1",
+        text: "first one"
+    },
+    {
+        id: "2",
+        text: "second one"
+    }
+]
+
 const typeDefs = gql`
     type User {
         id: ID!
@@ -10,7 +21,7 @@ const typeDefs = gql`
     type Tweet {
         id: ID!
         text: String!
-        author: User!
+        author: User
     }
     type Query {
         allTweets: [Tweet!]!
@@ -18,11 +29,38 @@ const typeDefs = gql`
     }
     type Mutation {
         postTweet(text: String!, userId: ID!): Tweet!
-        deleteTweet(id: id!): Boolean!
+        deleteTweet(id: ID!): Boolean!
     }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+    Query: {
+        allTweets() {
+            return tweets;
+        },
+        tweet(root, { id }) {
+            return tweets.find((tweet)=> tweet.id === id);
+        }
+    },
+    Mutation: {
+        postTweet(_, { text, userId }) {
+            const newTweet = {
+                id: tweets.length + 1,
+                text,
+            };
+            tweets.push(newTweet);
+            return newTweet
+        },
+        deleteTweet(_, { id }) {
+            const tweet = tweets.find(tweet => tweet.id === id);
+            if (!tweet) return false;
+            tweets = tweets.filter(tweet => tweet.id !== id);
+            return true;
+        }
+    },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
     console.log(`Running on ${url}`);
