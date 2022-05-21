122acb5ccd1505b19a919ad9f127d8c22bfab749
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2022-05-21 18:07:41 +0900

    Rest API를 graphQL로 묶는법
    타입을 설정해주고 리졸버에서 fetch를 이용하는 방법으로 json을 가져와 graphQL에서 사용할 수  있다.

--- tweetql/server.js
+++ tweetql/server.js
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from "apollo-server"
+import fetch from "node-fetch";
 
 let tweets = [
     {
@@ -49,6 +50,11 @@ const typeDefs = gql`
     }
     type Query {
         """
+        Get all Movies
+        """
+        allMovies: [Movie!]!
+        movie(id: String!): Movie
+        """
         Get all Users
         """
         allUsers: [User!]!
@@ -71,10 +77,43 @@ const typeDefs = gql`
         """
         deleteTweet(id: ID!): Boolean!
     }
+    type Movie {
+        id: Int!
+        url: String!
+        imdb_code: String!
+        title: String!
+        title_english: String!
+        title_long: String!
+        slug: String!
+        year: Int!
+        rating: Float!
+        runtime: Float!
+        genres: [String]!
+        summary: String
+        description_full: String!
+        synopsis: String
+        yt_trailer_code: String!
+        language: String!
+        background_image: String!
+        background_image_original: String!
+        small_cover_image: String!
+        medium_cover_image: String!
+        large_cover_image: String!
+      }
 `;
 
 const resolvers = {
     Query: {
+        allMovies() {
+            return fetch("https://yts.mx/api/v2/list_movies.json")
+            .then(r => r.json())
+            .then(json => json.data.movies);
+        },
+        movie(_, { id }) {
+            return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+            .then(r => r.json())
+            .then(json => json.data.movie);
+        },
         allTweets() {
             return tweets;
         },
